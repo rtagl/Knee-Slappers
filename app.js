b1920cd9a1cde500a418192333b1357ffce650b0
Ric Taglietti <rictaglietti@gmail.com>
2019-04-26 13:12:45 -0400

    fetch jokes from api

---
+++ app.js
@@ -0,0 +1,37 @@
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+function getJokes(e) {
+  e.preventDefault()
+  const number = document.querySelector('#number').value;
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+
+  xhr.onload = function() {
+    if(this.status === 200) {
+      const response = JSON.parse(this.responseText);
+
+      let output = '';
+
+      if(response.type === 'success') {
+        response.value.forEach((joke)=> {
+          output += 
+          `
+            <li>${joke.joke}</li>
+          `
+        })
+
+      } else {
+        output += '<li>Something went wrong.</li>'
+      }
+
+      document.querySelector('.jokes').innerHTML = output;
+
+    }
+
+  }
+
+  xhr.send()
+
+  document.querySelector('#number').value = '';
+}
